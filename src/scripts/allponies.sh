f294eaf20d385988897974be2c27653a3d9faf92
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2016-09-25 12:08:24 +0200

    generated a file containing all ponies

---
+++ src/scripts/allponies.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+inputfiles=( $@ )
+
+function getname()
+{
+    ponyname=$(basename $1 | sed "s/\.c//g" | sed "s/-/_/g")
+}
+function getreferences()
+{
+    for i in ${inputfiles[@]}; do
+        getname $i
+        echo "    ${2}${ponyname}_$1,"
+    done
+    echo "    0"
+}
+function Cfile
+{
+    echo "/* Auto Generated file DO NOT edit*/"
+    echo ""
+    for i in ${inputfiles[@]}; do
+        getname $i
+
+        echo "extern char ${ponyname}_topbottom;"
+        echo "extern char ${ponyname}_name[];"
+        echo "extern unsigned short ${ponyname}_balloon_top;"
+        echo "extern unsigned short ${ponyname}_balloon_bottom;"
+        echo "extern unsigned short ${ponyname}_width;"
+        echo "extern unsigned short ${ponyname}_height;"
+        echo "extern unsigned char ${ponyname}_pony[];"
+    done
+    echo "char* allponies_topbottom[]={"
+    getreferences topbottom '&'
+    echo "};"
+    echo "char* allponies_name[]={"
+    getreferences name
+    echo "};"
+    echo "unsigned short* allponies_balloon_top[]={"
+    getreferences  balloon_top '&'
+    echo "};"
+    echo "unsigned short* allponies_balloon_bottom[]={"
+    getreferences balloon_bottom '&'
+    echo "};"
+    echo "unsigned short* allponies_width[]={"
+    getreferences width '&'
+    echo "};"
+    echo "unsigned short* allponies_height[]={"
+    getreferences height '&'
+    echo "};"
+    echo "unsigned char* allponies_pony[]={"
+    getreferences pony
+    echo "};"
+    echo "int numPonies = ${#inputfiles[@]};"
+}
+function Hfile
+{
+    echo "/* Auto Generated file DO NOT edit*/"
+    echo ""
+    echo "extern char* allponies_topbottom[];"
+    echo "extern char* allponies_name[];"
+    echo "extern unsigned short* allponies_balloon_top[];"
+    echo "extern unsigned short* allponies_balloon_bottom[];"
+    echo "extern unsigned short* allponies_width[];"
+    echo "extern unsigned short* allponies_height[];"
+    echo "extern unsigned char* allponies_pony[];"
+    echo "extern int numPonies;"
+}
+Cfile > ponies.c
+Hfile > ponies.h
